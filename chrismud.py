d433e848216c73552d787026357cabe8160c5a7e
Chris Patti <feoh@feoh.org>
2025-01-19 17:26:25 -0500

    Put models into their own package.

--- chrismud.py
+++ chrismud.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
 from fastapi import FastAPI
-import models
+from models import Player, Thing
 
 sqlite_file_name='chrismud.db'
 sqlite_url='sqlite:///' + sqlite_file_name
@@ -16,6 +16,31 @@ app = FastAPI()
 @app.get("/player/create/{player_name}")
 def create_player(player_name: str):
     with Session(engine) as session:
-        player = models.Player(name=player_name)
+        player = Player(name=player_name)
         session.add(player)
         session.commit()
+@app.get("/player/delete/{player_id}")
+def delete_player(player_id: int):
+    with Session(engine) as session:
+        player = session.get(Player, player_id)
+        session.delete(player)
+        session.commit()
+
+@app.get("/player/get/{player_id}")
+def get_player(player_id: int):
+    with Session(engine) as session:
+        player = session.get(Player, player_id)
+        return player
+
+@app.get("/thing/create/{thing_name}")
+def create_thing(thing_name: str):
+    with Session(engine) as session:
+        thing = Thing(name=thing_name)
+        session.add(thing)
+        session.commit()
+
+@app.get("/thing/get/{thing_id}")
+def get_thing(thing_id: int):
+    with Session(engine) as session:
+        thing = session.get(Thing, thing_id)
+        return thing
